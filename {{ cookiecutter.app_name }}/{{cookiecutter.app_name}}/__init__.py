8a7e50d0129e1174b8ac4e647174223ff28beefd
Julian B <jeriox@users.noreply.github.com>
2021-03-21 00:03:43 +0100

    initialize template

---
+++ {{ cookiecutter.app_name }}/{{cookiecutter.app_name}}/__init__.py
@@ -0,0 +1,16 @@
+from ephios.core.plugins import PluginConfig
+
+
+class {{ cookiecutter.app_name }}App(PluginConfig):
+    name = "{{ cookiecutter.app_name }}"
+
+    class EphiosPluginMeta:
+        name = "{{ cookiecutter.app_name }}"
+        author = "{{ cookiecutter.author }}"
+        description = "{{ cookiecutter.description }}"
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = "{{ cookiecutter.app_name }}.{{ cookiecutter.app_name }}App"
